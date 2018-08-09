c9cd1b0c14480669db9826a0a6e473669d19cad0
zhenye163 <dzy930724@163.com>
2018-08-09 17:38:11 +0800

    在application.properties里面配置，打印SQL的预编译语句

--- src/main/java/com/netopstec/extensible/service/impl/StudentServiceImpl.java
+++ src/main/java/com/netopstec/extensible/service/impl/StudentServiceImpl.java
@@ -1,9 +1,13 @@
 package com.netopstec.extensible.service.impl;
 
+import com.netopstec.extensible.entity.Classroom;
 import com.netopstec.extensible.entity.Student;
+import com.netopstec.extensible.mapper.ClassroomMapper;
 import com.netopstec.extensible.mapper.StudentMapper;
 import com.netopstec.extensible.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -12,13 +16,30 @@ import java.util.List;
  * @author zhenye 2018/8/7
  */
 @Service
+@Slf4j
 public class StudentServiceImpl implements StudentService{
 
     @Resource
     private StudentMapper studentMapper;
+    @Resource
+    private ClassroomMapper classroomMapper;
 
     @Override
     public List<Student> findAll() {
         return studentMapper.findAll();
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public String insertOne(Student student) {
+        Integer classroomId = student.getClassroomId();
+        Classroom classroom = classroomMapper.findById(classroomId);
+        if (classroom == null){
+            log.error("找不到这个班级");
+            int a = 1 / 0;
+        }
+        Integer ID = studentMapper.insertOne(student);
+
+        return null;
     }
 }
